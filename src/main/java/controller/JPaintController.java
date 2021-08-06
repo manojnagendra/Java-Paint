089e0a8751b56800f5b809918a937644c3024989
manojnagendra <manojnagendra@gmail.com>
2021-08-06 10:38:47 -0500

    Update JPaintController.java

--- src/main/java/controller/JPaintController.java
+++ src/main/java/controller/JPaintController.java
@@ -1,10 +1,11 @@
 package controller;
 
-import model.Geometry;
+import controller.copypastedelete.CopyTheShapes;
+import controller.copypastedelete.DeleteTheShapes;
+import controller.copypastedelete.PasteTheShapes;
 import model.GeometryConfig;
 import model.interfaces.IApplicationState;
 import model.interfaces.InterfaceofShapes;
-import model.persistence.ApplicationState;
 import view.EventName;
 import view.interfaces.IUiModule;
 
@@ -32,8 +33,10 @@ public class JPaintController implements IJPaintController {
         uiModule.addEvent(EventName.CHOOSE_SECONDARY_COLOR, () -> iApplicationState.setActiveSecondaryColor());
         uiModule.addEvent(EventName.CHOOSE_SHADING_TYPE, () -> iApplicationState.setActiveShadingType());
         uiModule.addEvent(EventName.CHOOSE_MOUSE_MODE, () -> iApplicationState.setActiveStartAndEndPointMode());
-
         uiModule.addEvent(EventName.UNDO, () -> new Undo().execute());
         uiModule.addEvent(EventName.REDO, () -> new Redo().execute());
+        uiModule.addEvent(EventName.COPY, () -> new CopyTheShapes(iApplicationState,geometry, geometryConfig).execute());
+        uiModule.addEvent(EventName.PASTE, () -> new PasteTheShapes(iApplicationState,geometry, geometryConfig).execute());
+        uiModule.addEvent(EventName.DELETE, () -> new DeleteTheShapes(iApplicationState,geometry, geometryConfig).execute());
     }
 }
