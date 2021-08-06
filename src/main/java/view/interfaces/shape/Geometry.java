58edb0df18a761d99d02104dc050b9f61bacb821
manojnagendra <manojnagendra@gmail.com>
2021-08-06 10:44:50 -0500

    Update Geometry.java

--- src/main/java/view/interfaces/shape/Geometry.java
+++ src/main/java/view/interfaces/shape/Geometry.java
@@ -9,14 +9,14 @@ public interface Geometry {
 
     void draw(Graphics graph);
     boolean contains(MouseCoordinates point);
-    MouseCoordinates getPoint();
-    MouseCoordinates getPoint1();
+    MouseCoordinates getThefirstpoint();
+    MouseCoordinates getThesecondpoint();
     void sumCoord(int deltacoord);
     void sumCoord1(int deltacoord1);
-    void setEnd(MouseCoordinates End);
-    void setStart(MouseCoordinates Start);
-    MouseCoordinates getStart();
-    MouseCoordinates getEnd();
+    void setEndingpoint(MouseCoordinates End);
+    void setStartingpoint(MouseCoordinates Start);
+    MouseCoordinates getStartingpoint();
+    MouseCoordinates getEndingpoint();
     GeometryConfig getGeometryConfig();
     int getShapeWidth();
     int getShapeHeight();
